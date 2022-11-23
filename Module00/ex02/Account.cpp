06e0bd1e0157918de6c9a832ac68608756829cec
walidboutzougarte <walidboutzougarte@gmail.com>
2022-11-23 15:55:17 +0100

    add dispaytimestamp method and add log message in constracter

--- Module00/ex02/Account.cpp
+++ Module00/ex02/Account.cpp
@@ -3,7 +3,11 @@
 
 Account::Account(int initial_deposit)
 {
+    static int a;
+    _accountIndex = a++;
     this->_totalAmount = initial_deposit;
+    _displayTimestamp();
+    std::cout << "index:" << _accountIndex << ";amount:" << _totalAmount << ";created" << std::endl;
 }
 
 static int	getNbAccounts( void ) 
@@ -26,8 +30,13 @@ static int	getNbWithdrawals( void )
     return _totalNbWithdrawals;
 }
 
+static void _displayTimestamp(void)
+{
+    std::time_t  t = std::time(0);
+    std::tm* now = std::localtime(&t);
+    std::cout << "[" << now->tm_year + 1900 << now->tm_mon << now->tm_mday << "_"  << now->tm_hour << now->tm_min << now->tm_sec << "] ";
+}
+
 static int	displayAccountsInfos( void ) 
 {
-   
-    
 }
